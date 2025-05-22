913f7e9a9a947713893360710bc77cd01958a5b4
crushingismybusiness <crushingismybusiness@andbusinessisgood>
2025-05-21 17:26:10 -0700

    Add a helper method to merge a dataframe into a deltatable
    ```
    $ docker compose exec test-container bash
    spark_user@b55add55ebd8:/imp_test$ ipython
    *snip*
    
    In [1]: from utils.spark import SparkProvider
    
    In [2]: from pyspark.sql.types import StructType, IntegerType,
    StructField, Stri
          ⋮ ngType
    
    In [3]: prov = SparkProvider("myapp")
    
    In [4]: spark = prov()
    25/05/22 00:14:11 WARN NativeCodeLoader: Unable to load native-hadoop
    library for your platform... using builtin-java classes where applicable
    Setting default log level to "WARN".
    To adjust logging level use sc.setLogLevel(newLevel). For SparkR, use
    setLogLevel(newLevel).
    25/05/22 00:14:12 WARN Utils: spark.executor.instances less than
    spark.dynamicAllocation.minExecutors is invalid, ignoring its setting,
    please update your configs.
    
    In [5]: spark.sql("CREATE DATABASE testing")
    Out[5]: DataFrame[]
    
    In [6]: spark.sql("""
       ...:     CREATE TABLE testing.minions (
       ...:         emp_id INT,
       ...:         emp_name STRING
       ...:     )
       ...:     USING DELTA
       ...:     """
       ...: )
    
    Out[6]: DataFrame[]
    
    In [7]: schema = StructType([
       ...:     StructField("emp_id", IntegerType(), nullable=False),
       ...:     StructField("emp_name", StringType(), nullable=False)
       ...: ])
    
    In [8]: df = spark.createDataFrame([(1, "foo"), (2, "bar")],
    schema=schema)
    
    In [9]: from cdmeventimporters.utilities import
    merge_spark_df_to_deltatable
    
    In [11]: merge_spark_df_to_deltatable(
        ...:     spark,
        ...:     df,
        ...:     "testing.minions",
        ...:     "target.emp_id = source.emp_id",
        ...: )
    
    In [12]: newdf = spark.sql("SELECT * FROM testing.minions")
    
    In [13]: newdf.show()
    +------+--------+
    |emp_id|emp_name|
    +------+--------+
    |     2|     bar|
    |     1|     foo|
    +------+--------+
    
    In [14]: df = spark.createDataFrame([(1, "drop"), (3, "keep")],
    schema=schema)
    
    In [15]: merge_spark_df_to_deltatable(
        ...:     spark,
        ...:     df,
        ...:     "testing.minions",
        ...:     "target.emp_id = source.emp_id",
        ...: )
    
    In [16]: newdf = spark.sql("SELECT * FROM testing.minions")
    
    In [17]: newdf.show()
    +------+--------+
    |emp_id|emp_name|
    +------+--------+
    |     3|    keep|
    |     2|     bar|
    |     1|     foo|
    +------+--------+
    
    In [18]: df = spark.createDataFrame([(1, "replace"), (4, "hithere")],
    schema=sch
           ⋮ ema)
    
    In [22]: merge_spark_df_to_deltatable(
        ...:     spark,
        ...:     df,
        ...:     "testing.minions",
        ...:     "target.emp_id = source.emp_id",
        ...:     update=True
        ...: )
    
    In [23]: newdf = spark.sql("SELECT * FROM testing.minions")
    
    In [24]: newdf.show()
    +------+--------+
    |emp_id|emp_name|
    +------+--------+
    |     1| replace|
    |     4| hithere|
    |     3|    keep|
    |     2|     bar|
    +------+--------+
    ```

--- cdmeventimporters/utilities.py
+++ cdmeventimporters/utilities.py
@@ -1,3 +1,53 @@
 """
 General utilities useful for importing data into the CDM.
 """
+
+from delta.tables import DeltaTable
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+
+from cdmeventimporters.arg_checkers import (
+    not_falsy as _not_falsy,
+    require_string as _require_string
+)
+
+
+def merge_spark_df_to_deltatable(
+        spark: SparkSession,
+        df: DataFrame,
+        full_table_name: str,
+        merge_condition: str,
+        update: bool = False,
+        target: str = "target",
+        source: str = "source",
+    ):
+    """
+    Merge a Spark DataFrame into an existing deltatable.
+    
+    spark - a SparkSession configured with deltatable support.
+    df - the DataFrame to merge. Its schema must match that of the table.
+    full_table_name - the name of the table, in <database>.<table> format.
+    merge_condition - the condition that will be used to detect equivalent rows where the row
+        in the dataframe should be dropped if it already exists in the deltatable. For example,
+        `"target.employee_id = source.employee_id"`
+    update - instead of dropping equivalent rows in the dataframe, replace the rows in the
+        deltatable with the row in the dataframe.
+    target - the alias of the target deltatable to use in the merge condition string..
+    source - the alias of the source dataframe to use in the merge condition string.
+    """
+    _not_falsy(spark, "spark")
+    _not_falsy(df, "df")
+    _require_string("full_table_name", full_table_name)
+    _require_string("merge_condition", merge_condition)
+    _require_string("target", target)
+    _require_string("source", source)
+    
+    delta_table = DeltaTable.forName(spark, full_table_name)
+    preex = delta_table.alias(
+        target
+        ).merge(source=df.alias(source), condition=merge_condition
+        ).whenNotMatchedInsertAll(
+    )
+    if update:
+        preex = preex.whenMatchedUpdateAll()
+    preex.execute()
